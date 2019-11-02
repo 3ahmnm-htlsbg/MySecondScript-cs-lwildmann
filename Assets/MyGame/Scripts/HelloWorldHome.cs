dd41cfa7438776cf7f91c9f52563e19b49f8591f
lwildmann <wildmannlukas03@gmail.com>
2019-11-02 14:51:48 +0100

    ChangedScript

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class HelloWorldHome : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
